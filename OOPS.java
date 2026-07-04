4cb66b7cda1ca8288281b127447359177f393682
Saransh-raj <saranshraj011@gmail.com>
2026-07-04 23:45:09 +0530

    adding comments

--- OOPS.java
+++ OOPS.java
@@ -1,5 +1,6 @@
 public class OOPS {
 
+    // class :- class is a blueprint of an object
     public static class Student {
         String name;
         int roll;
@@ -7,7 +8,7 @@ public class OOPS {
     }
     public static void main(String[] args) {
 
-        
+        // object is an real world entity
         Student s1 = new Student(); // s1 is a student 1 object
         s1.name = "Saransh Raj"; // student 1 assigning name
         s1.roll = 23;   // student 1 assigning roll
@@ -16,10 +17,10 @@ public class OOPS {
         System.out.println("Name : "+s1.name + ", roll : "+s1.roll+", CGPA : "+s1.CGPA);
 
 
-        Student s2 = new Student(); // s1 is a student 1 object
-        s2.name = "Jaghvi"; // student 1 assigning name
-        s2.roll = 22;   // student 1 assigning roll
-        s2.CGPA = 9.01; // student 1 assigning CGPA
+        Student s2 = new Student(); // s2 is a student 2 object
+        s2.name = "Jaghvi"; // student 2 assigning name
+        s2.roll = 22;   // student 2 assigning roll
+        s2.CGPA = 9.01; // student 2 assigning CGPA
 
         System.out.println("Name : "+s2.name + ", roll : "+s2.roll+", CGPA : "+s2.CGPA);
     }
