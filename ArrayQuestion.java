970a05f7cdca79e3feba596e08d0ccc022f8fa59
Saransh-raj <saranshraj011@gmail.com>
2026-07-08 23:02:30 +0530

    swapping using swap method

--- ArrayQuestion.java
+++ ArrayQuestion.java
@@ -1,5 +1,14 @@
 import java.util.*;
 public class ArrayQuestion {
+
+    // swap method
+    static void swap(int a,int b){
+        int temp = a;
+        a = b;
+        b = temp;
+
+        System.out.println(a+" , "+b);
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         // return the first value that is repeating in this array if no value is being repeated , return -1
@@ -17,6 +26,13 @@ public class ArrayQuestion {
             if(result != -1) break; // stop outer loop once found
         }
         System.out.println(result);
+
+        // swapping a and b using swap method
+        int a = 10;
+        int b = 20;
+        swap(a, b); // swap method call
+
+        
         sc.close();
     }
 }
