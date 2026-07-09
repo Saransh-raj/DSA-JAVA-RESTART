921a4eab128b54459b65fb105d3d2be883aeda12
Saransh-raj <saranshraj011@gmail.com>
2026-07-09 23:26:12 +0530

    Prefix Sum

---
+++ PrefixSum.java
@@ -0,0 +1,15 @@
+public class PrefixSum {
+    public static void main(String[] args) {
+        int[] arr = {2,1,3,4,5}; // prefix sum = {2,3,6,10,15};
+        int[] pref = new int[arr.length];
+        pref[0] = arr[0]; 
+
+        for(int i=1;i<arr.length;i++){
+            pref[i] = pref[i-1] + arr[i];
+        }
+
+        for(int num : pref){
+            System.out.print(num +" ");
+        }
+    }
+}
