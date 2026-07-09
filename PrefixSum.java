db3f0352dc52a93956ef6c89f132cbaae8a1835b
Saransh-raj <saranshraj011@gmail.com>
2026-07-09 23:35:45 +0530

    prefix sum without using another array

--- PrefixSum.java
+++ PrefixSum.java
@@ -1,5 +1,7 @@
 public class PrefixSum {
     public static void main(String[] args) {
+
+        // prefix sum using new array
         int[] arr = {2,1,3,4,5}; // prefix sum = {2,3,6,10,15};
         int[] pref = new int[arr.length];
         pref[0] = arr[0]; 
@@ -9,6 +11,17 @@ public class PrefixSum {
         }
 
         for(int num : pref){
+            System.out.print(num +" ");
+        }
+
+        System.out.println();
+
+        // prefix sum without using another array
+        for(int i=1;i<arr.length;i++){
+            arr[i] = arr[i-1] + arr[i];
+        }
+        
+        for(int num : arr){
             System.out.print(num +" ");
         }
     }
