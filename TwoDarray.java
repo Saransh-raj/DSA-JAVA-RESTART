1b64e30731bf088dc378df6b050fec4c8dc0b136
Saransh-raj <saranshraj011@gmail.com>
2026-07-11 01:12:57 +0530

    2D array user input and printing the elements

--- TwoDarray.java
+++ TwoDarray.java
@@ -13,11 +13,36 @@ public class TwoDarray {
         // printing the 2d array
         for(int i=0;i<arr.length;i++){ // row 
             for (int j = 0; j < arr[i].length; j++) {   // column
-                System.out.print    (arr[i][j]+ " ");
+                System.out.print(arr[i][j]+ " ");
             }
             System.out.println();
         }
+
+
+        // 2D array another syntax and taking user input
+        System.out.println("Enter row : ");
+        int row = sc.nextInt();
+        System.out.println("Enter column : ");
+        int col = sc.nextInt();
+        int[][] array = new int[row][col];
+
+        // taking user input in 2D array
+        System.out.println("Enter "+row*col+" elements");
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                array[i][j] = sc.nextInt();
+            }
+        }
+
         
+        // printing the elements of the 2D array
+        System.out.println("You enter elements:- ");
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                System.out.print(array[i][j]+ " ");
+            }
+            System.out.println();
+        }
         sc.close();
     }
 }
