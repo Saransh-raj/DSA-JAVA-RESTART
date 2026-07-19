1cecd002979a55b7a85930ca453d086e1886221e
Saransh-raj <saranshraj011@gmail.com>
2026-07-19 23:32:47 +0530

    remove and reverse method in java

--- ArrayListBasic.java
+++ ArrayListBasic.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Collections;
 public class ArrayListBasic {
     public static void main(String[] args) {
         
@@ -22,6 +23,14 @@ public class ArrayListBasic {
 
         arr.set(2, 10); // set the value of the list at index 2 - 10
         System.out.println(arr.get(2)); // 10
-        
+        System.out.println(arr); // it will print the whole array like :- [10, 20, 10, 40, 50]
+
+        //remove method
+        arr.remove(2);
+        System.out.println(arr);
+
+        // reverse method
+        Collections.reverse(arr);
+        System.out.println(arr);
     }
 }
