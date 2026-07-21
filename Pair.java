2a2729baa4ce349ecaa17b8e3dacd09bee46db2b
Saransh-raj <saranshraj011@gmail.com>
2026-07-21 17:51:55 +0530

    Count total number of pairs

--- Pair.java
+++ Pair.java
@@ -8,13 +8,16 @@ public class Pair {
                 // (3,4) (3,5)
                 // (4,5)
 
+        int totalPairs = 0;
         int[] arr = {1,2,3,4,5};
         int n = arr.length;
         for(int i=0;i<n;i++){
             for(int j=i+1;j<n;j++){
                 System.out.print("("+arr[i]+","+arr[j]+")");
+                totalPairs++;
             }
             System.out.println();
         }
+        System.out.println(totalPairs);
     }
 }
