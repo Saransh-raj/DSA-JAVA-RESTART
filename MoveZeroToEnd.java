6f1c5542da613e8eedcb6a0ce004307f6b8476b9
Saransh-raj <saranshraj011@gmail.com>
2026-07-23 11:13:17 +0530

    Moves zeros to end using bubble sort

---
+++ MoveZeroToEnd.java
@@ -0,0 +1,21 @@
+public class MoveZeroToEnd {
+    public static void printArr(int[] arr){
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+    public static void main(String[] args) {
+        int[] arr = {1,0,2,3,0,4,8,0,10,12};
+        int n = arr.length;
+        for(int i=0;i<n-1;i++){
+            for(int j=0;j<n-1-i;j++){
+                if(arr[j] == 0){
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+        printArr(arr);
+    }
+}
