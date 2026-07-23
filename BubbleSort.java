a685d3b6c6d3eeef809c6d1007b243eaee217751
Saransh-raj <saranshraj011@gmail.com>
2026-07-23 09:49:24 +0530

    Bubble sort all approach

--- BubbleSort.java
+++ BubbleSort.java
@@ -8,7 +8,7 @@ public class BubbleSort {
         int[] arr = {5,3,8,4,2};
         int n = arr.length;
 
-        // brute force approach T.C :- O(n)
+        // brute force approach T.C :- O(n^2)
         for(int i=0;i<=n-1;i++){
             for(int j=0;j<n-1;j++){
                 if(arr[j]>arr[j+1]) {
@@ -19,5 +19,42 @@ public class BubbleSort {
         }
     }
         printArr(arr);
+        System.out.println();
+    
+    // Average case approach T.C :- O(n^2)
+        for(int i=0;i<=n-1;i++){
+            for(int j=0;j<n-1-i;j++){ // goes upto  n - 1 - i
+                if(arr[j]>arr[j+1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+            printArr(arr);
+            System.out.println();
+
+    // Best case approach T.C :- O(n)
+        for(int i = 0; i < n - 1; i++) {
+
+        boolean swapped = false;
+
+            for(int j = 0; j < n - 1 - i; j++) {
+
+                if(arr[j] > arr[j + 1]) {
+
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+
+                    swapped = true;
+                }
+            }
+
+            if(!swapped)
+                break;
+        }
+        printArr(arr);
+        System.out.println();
     }
 }
